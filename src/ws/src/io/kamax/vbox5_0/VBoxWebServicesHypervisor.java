c242d846d4ec507cb1ec12c7b0fcbe6b0c8a669d
Maxime Dor <max@kamax.io>
2015-04-02 01:28:58 +0200

    Updated headers

--- src/ws/src/io/kamax/vbox5_0/VBoxWebServicesHypervisor.java
+++ src/ws/src/io/kamax/vbox5_0/VBoxWebServicesHypervisor.java
@@ -1,8 +1,8 @@
 /*
- * Hyperbox - Enterprise Virtualization Manager
+ * Hyperbox - Virtual Infrastructure Manager
  * Copyright (C) 2015 - Maxime Dor
  * 
- * http://hyperbox.altherian.org
+ * http://kamax.io/hbox/
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,12 +11,11 @@
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package io.kamax.vbox5_0;
